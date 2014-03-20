061d544b5fc93aef60db430fc606f429b6ea7e0d
Bruno Imbrizi <bruno.imbrizi@unit9.com>
2014-03-20 14:34:39 +0100

    added example-assimp-model.cbp

--- example-assimp-model/src/main.cpp
+++ example-assimp-model/src/main.cpp
@@ -6,7 +6,9 @@ int main()
 {
 	ofAppGlutWindow window; // create a window
 	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
+	#ifdef __APPLE__
     window.setGlutDisplayString("rgba double samples>=8 depth");
+    #endif
 	ofSetupOpenGL(&window, 1280, 720, OF_WINDOW);
 	ofRunApp(new testApp()); // start the app
 }
